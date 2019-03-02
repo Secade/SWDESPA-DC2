73fdfd2859bc8d7d45807fe4cae948711afe8fab
Jeremy Dale Coronia <jeremy_coronia@dlsu.edu.ph>
2019-03-03 03:15:48 +0800

    Added 5 songs and base code for playing, pausing, stopping music.

---
+++ src/view/MedPlayer.java
@@ -0,0 +1,43 @@
+package view;
+
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.io.*;
+
+public class MedPlayer {
+
+    private String file;
+    private Media hit;
+    private MediaPlayer mediaPlayer;
+    private MediaView music;
+
+    public MedPlayer(File filename){
+        final JFXPanel fxpanel = new JFXPanel();
+        file = "C:\\Users\\LENOVO\\GitHub Repository\\SWDESPA-DC2\\src\\audio\\" + filename + ".mp3";
+        hit = new Media(new File(file).toURI().toString());
+        mediaPlayer = new MediaPlayer(hit);
+        music = new MediaView(mediaPlayer);
+    }
+
+    public void play(){
+        mediaPlayer.play();
+        System.out.println("played");
+    }
+
+    public void pause(){
+        mediaPlayer.pause();
+    }
+
+    public void stop(){
+        mediaPlayer.stop();
+    }
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("Nelly Furtado - Say It Right");
+        MedPlayer me = new MedPlayer(file);
+        me.play();
+    }
+}
