15d468847d91ab3d2f36927249ad7c5873ca0047
melodyjoygo <47266191+melodyjoygo@users.noreply.github.com>
2019-03-14 07:21:15 +0800

    added songcount

--- src/model/UserWithSongService.java
+++ src/model/UserWithSongService.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static controller.MainController.loggedin;
+
 public class UserWithSongService {
     private Database db;
 
@@ -17,13 +19,14 @@ public class UserWithSongService {
     public boolean add(UserWithSong u){
         // ADD CONTACT
 
-        String query = "INSERT INTO " + UserWithSong.TABLE_NAME + " VALUE (?, ?)";
+        String query = "INSERT INTO " + UserWithSong.TABLE_NAME + " VALUE (?, ?, ?)";
         Connection connection = db.getConnection();
 
         try{
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setInt(1,u.getUserID());
             statement.setInt(2,u.getSongID());
+            statement.setInt(3,0);
 
             boolean added = statement.execute();
             return added;
@@ -38,7 +41,8 @@ public class UserWithSongService {
         Connection connection = db.getConnection();
         List<UserWithSong> userwithsongs = new ArrayList<>();
 
-        String query = "SELECT * FROM " + UserWithSong.TABLE_NAME ;
+        String query = "SELECT * FROM " + UserWithSong.TABLE_NAME
+                    +"WHERE " +UserWithSong.COL_USERID+ "=" + loggedin.getId() ;
 
         try {
             PreparedStatement statement = connection.prepareStatement(query);
@@ -48,6 +52,7 @@ public class UserWithSongService {
                 UserWithSong uws = new UserWithSong();
                 uws.setUserID(rs.getInt(UserWithSong.COL_USERID));
                 uws.setSongID(rs.getInt(UserWithSong.COL_SONGID));
+                uws.setPlaycount(rs.getInt(UserWithSong.COL_PLAYCOUNT));
                 userwithsongs.add(uws);
             }
 
@@ -57,5 +62,58 @@ public class UserWithSongService {
 
         return userwithsongs;
     }
+    private boolean updatePlayCount(int songID){
+        String query = "UPDATE " + Song.TABLE_NAME + " SET "
+                    + UserWithSong.COL_PLAYCOUNT +"="+ UserWithSong.COL_PLAYCOUNT+"+1"
 
-}
+                    +"WHERE " + UserWithSong.COL_USERID + "=" + loggedin.getId() +"AND "+ UserWithSong.COL_SONGID +"=" + songID  ;
+
+        Connection connection = db.getConnection();
+
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            boolean added = statement.execute();
+            return added;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private Song getMostPlayed(){
+        Connection connection = db.getConnection();
+        Song s = new Song();
+
+        String query = "SELECT " +Song.TABLE_NAME + "." + Song.COL_SONGID +","+ Song.TABLE_NAME + "." + Song.COL_SONGTITLE +","+Song.TABLE_NAME + "." + Song.COL_GENRE+","+ Song.TABLE_NAME + "." + Song.COL_ALBUM +","+ Song.TABLE_NAME + "." + Song.COL_ARTIST+","+ Song.TABLE_NAME + "." + Song.COL_YEAR+","+ Song.TABLE_NAME + "." + Song.COL_DURATION+","+ Song.TABLE_NAME + "." + Song.COL_FILENAME+
+                        "FROM " +Song.TABLE_NAME + "RIGHT JOIN "+ UserWithSong.TABLE_NAME +
+                        "ON " +Song.TABLE_NAME+"."+Song.COL_SONGID +"="+ UserWithSong.TABLE_NAME+"."+UserWithSong.COL_SONGID +
+                        "WHERE " +UserWithSong.TABLE_NAME+"."+UserWithSong.COL_USERID +"="+ loggedin.getId() +"AND " +UserWithSong.TABLE_NAME+"."+UserWithSong.COL_PLAYCOUNT+"="+
+                        "(SELECT MAX(" +UserWithSong.TABLE_NAME+"."+UserWithSong.COL_PLAYCOUNT+ ") FROM " +UserWithSong.TABLE_NAME + "WHERE "+ UserWithSong.COL_USERID +"="+ loggedin.getId()+")";
+
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                s.setSongID(rs.getInt(Song.COL_SONGID));
+                s.setSongTitle(rs.getString(Song.COL_SONGTITLE));
+                s.setGenre(rs.getString(Song.COL_GENRE));
+                s.setAlbum(rs.getString(Song.COL_ALBUM));
+                s.setAlbum(rs.getString(Song.COL_ARTIST));
+                s.setYear(rs.getInt(Song.COL_YEAR));
+                s.setDuration(rs.getFloat(Song.COL_DURATION));
+                s.setFilename(rs.getString(Song.COL_FILENAME));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return s;
+    }
+
+    }
+
+
